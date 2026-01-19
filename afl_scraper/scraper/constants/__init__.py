81a989bb994d7e7dc52bf8a4e89a97670a564a86
Tom <twdickman@gmail.com>
2026-01-19 20:42:17 +1100

    feat: Add ability to scrape player links for year

--- afl_scraper/scraper/constants/__init__.py
+++ afl_scraper/scraper/constants/__init__.py
@@ -1,3 +1,4 @@
 from .css_selectors_fixture import FIXTURE_CLASSNAMES
+from .css_selectors_stats import STATS_CLASSNAMES
 from .paths import PATHS
 from .season_ids import SEASON_ID
--- afl_scraper/scraper/parser/__init__.py
+++ afl_scraper/scraper/parser/__init__.py
@@ -1,1 +1,2 @@
 from .match import display_player_stats, extract_table_data
+from .players import scrape_players_links
